9f2a8500046ee7f73545ef0985629f7e9ed12b38
nepa27 <nepalek@mail.ru>
2025-05-07 20:20:26 +0300

    Change work of main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from selenium_utils import login_hh_ru
+from selenium_utils import driver_init, login_hh_ru, get_message_from_chats
+from parser_utils import parse_chats_url
 
 
 def main():
     """Основная функция."""
-    login_hh_ru()
-
+    driver = None
+    try:
+        driver = driver_init()
+        login_hh_ru(driver)
+        chats_data = parse_chats_url()
+        get_message_from_chats(chats_data, driver)
+    except BaseException as er:
+        print(f'Возникла ошибка в {__name__}: {er}')
+    finally:
+        if driver:
+            driver.quit()
 
 if __name__ == '__main__':
     main()
