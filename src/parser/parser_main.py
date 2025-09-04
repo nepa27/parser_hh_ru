1ba4d851af114c4f8a032be96e40c5388242fe0d
nepa27 <nepalek@mail.ru>
2025-09-04 16:35:46 +0300

    Change structure of main; Add docstring for function

--- src/parser/parser_main.py
+++ src/parser/parser_main.py
@@ -1,20 +1,42 @@
-from selenium_utils import driver_init, login_hh_ru, get_message_from_chats
-from parser_utils import parse_chats_url
+from src.parser.selenium_worker import SeleniumUtils
+from src.parser.parser_utils import parse_chats_url
+from src.parser.config import ParserConfig
 
+from src.logger_config import logger
 
-def main():
-    """Основная функция."""
-    driver = None
+def main() -> None:
+    """
+    Основная функция работы парсера.
+
+    Определяем экземпляр класса SeleniumUtils.
+    Инициализируем драйвер для дальнейшей работы driver_init().
+    Аутентифицируемся на HH.ru auth_hh_ru().
+    Переходим на страницу с личными чатами соискателя move_to_chat().
+
+    Сохраням данные чатов в виде HTML страницы и парсим уже её,
+    чтобы не работать напрямую с браузером.
+    Парсим чаты parse_chats_url().
+    Далее проходим по каждому чату и получаем данные get_message_from_chats().
+
+    """
     try:
-        driver = driver_init()
-        login_hh_ru(driver)
-        chats_data = parse_chats_url()
-        get_message_from_chats(chats_data, driver)
+        config = ParserConfig()
+
+        selenium_worker = SeleniumUtils(
+            config,
+            logger
+        )
+        selenium_worker.driver_init()
+        selenium_worker.auth_hh_ru()
+        selenium_worker.move_to_chat()
+
+        chats_data: list = parse_chats_url()
+        selenium_worker.get_message_from_chats(chats_data)
+
     except BaseException as er:
-        print(f'Возникла ошибка в {__name__}: {er}')
+        logger.error(f'Возникла ошибка в {__name__}: {er}')
     finally:
-        if driver:
-            driver.quit()
+        selenium_worker.quit_driver()
 
 
 if __name__ == '__main__':
