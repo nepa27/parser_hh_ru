acf5dcfb9871d15772fe7ef70556a08d67201088
nepa27 <nepalek@mail.ru>
2025-09-11 15:29:51 +0300

    Create base config of database

---
+++ src/core/database.py
@@ -0,0 +1,27 @@
+from os import getenv
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+
+load_dotenv()
+
+DB_HOST = getenv('DB_HOST')
+DB_PORT = getenv('DB_PORT')
+DB_USER = getenv('DB_USER')
+DB_PASS = getenv('DB_PASS')
+DB_NAME = getenv('DB_NAME')
+
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+async_engine = create_async_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+async_session = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False
+)
