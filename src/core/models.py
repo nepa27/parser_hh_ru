5fb14f776d4651e385d34a8b840f6988b26088e0
nepa27 <nepalek@mail.ru>
2025-09-11 15:30:47 +0300

    Change config for field of password

--- src/core/models.py
+++ src/core/models.py
@@ -13,7 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, nullable=False, unique=True)
     second_name = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
     email = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=False)
 
