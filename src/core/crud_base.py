beec5c889048bc893e1d7d4f9c10fe71d39c751a
nepa27 <nepalek@mail.ru>
2025-09-12 15:35:03 +0300

    Add create and update methods

--- src/core/crud_base.py
+++ src/core/crud_base.py
@@ -2,12 +2,12 @@ from typing import Type, Sequence
 
 import asyncio
 from fastapi import HTTPException, status
-from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
 from sqlalchemy.sql import text
 
-from src.core.database import async_engine, async_session, get_db
+from src.core.config.logging import logger
+from src.core.database import async_engine, get_db
 from src.core.models import Base, User
 
 
@@ -25,84 +25,125 @@ class CRUDBase:
 
     async def get_all(
             self,
-            db: AsyncSession
+            session: AsyncSession
     ) -> Sequence[Base]:
         """Получает данные из БД."""
-        query = await db.execute(select(self.model))
+        query = await session.execute(select(self.model))
         result = query.scalars().all()
+        # TODO: Удалить позже
+        for res in result:
+            print((res.id, res.second_name))
         return result
 
     async def get_or_404(
             self,
             obj_id: int,
-            db: AsyncSession
+            session: AsyncSession
     ) -> Base:
         """Получает объект из БД по id или выбрасывает 404-ошибку."""
-        query = await db.execute(
+        query = await session.execute(
             select(self.model).where(self.model.id == obj_id)
         )
         result = query.scalars().first()
         if not result:
+            message = f'Объект {obj_id} в {self.model.__tablename__} не найден'
+            logger.error(message)
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail=f'Объект {obj_id} в {self.model.__tablename__} не найден')
+                detail=message)
         return result
 
-
-async def check_db_connection(engine: AsyncEngine) -> None:
-    try:
-        async with engine.connect() as connection:
-            result = await connection.execute(text('SELECT version();'))
-            db_version = result.scalar_one()
-            print(f'База данных подключена. Версия: {db_version}')
-    except Exception as e:
-        print(f'Ошибка при подключении к БД: {e}')
-
-
-async def create_tables(engine: AsyncEngine) -> None:
-    """Функция создания таблиц."""
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def delete_tables(engine: AsyncEngine) -> None:
-    """Функция удаления таблиц."""
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-
-
-async def create_data(model: Type[Base], data: dict) -> None:
-    """Функция добавления данных в таблицу."""
-    async with async_session() as session:
+    async def create(
+            self,
+            session: AsyncSession,
+            data: dict
+    ) -> Base:
+        """Добавляет данные в БД."""
+        db_obj = self.model(**data)
         try:
-            session.add(model(**data))
+            session.add(db_obj)
             await session.commit()
-        except IntegrityError as e:
-            detail = (e.args[-1]).split('DETAIL:')[-1].strip()
-            print(f'Ошибка при добавлении данных. {detail}\n')
+            await session.refresh(db_obj)
+
+            return db_obj
+        except Exception as e:
+            logger.error(f'Ошибка при добавлении данных в БД: {e}')
             await session.rollback()
+
+    async def update(
+            self,
+            session: AsyncSession,
+            data: dict,
+            obj_id: int
+    ) -> None:
+        """Обновляет данные в БД."""
+        query = (
+            update(self.model)
+            .values(**data)
+            .filter_by(id=obj_id)
+        )
+        await session.execute(query)
+        await session.commit()
+
+    @staticmethod
+    async def check_db_connection(engine: AsyncEngine) -> None:
+        try:
+            async with engine.connect() as connection:
+                result = await connection.execute(text('SELECT version();'))
+                db_version = result.scalar_one()
+                print(f'База данных подключена. Версия: {db_version}')
+        except Exception as e:
+            print(f'Ошибка при подключении к БД: {e}')
+
+    @staticmethod
+    async def create_tables(engine: AsyncEngine) -> None:
+        """Функция создания таблиц."""
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    @staticmethod
+    async def delete_tables(engine: AsyncEngine) -> None:
+        """Функция удаления таблиц."""
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
 
 
 async def main() -> None:
-    await check_db_connection(async_engine)
-
     # await delete_tables(async_engine)
     # await create_tables(async_engine)
 
     # await create_data(
     #     User,
-    #     {
-    #         'first_name': 'name2',
-    #         'second_name': 'name2',
-    #         'password': 'password',
-    #         'email': 'email2',
-    #         'is_active': True
-    #     }
+    data = {
+        'first_name': 'name3',
+        'second_name': 'name3',
+        'password': 'password',
+        'email': 'email3',
+        'is_active': True
+    }
+    new_data = {
+        'second_name': 'name56',
+    }
+
     # )
     async for session in get_db():
         test_ex = CRUDBase(User)
+        await test_ex.check_db_connection(async_engine)
+
+        # await test_ex.get_all(session)
+        # await test_ex.get_or_404(1, session)
+        #
+        # await test_ex.create(
+        #     session,
+        #     data
+        # )
+        # await test_ex.get_all(session)
+        # await test_ex.update(
+        #     session,
+        #     new_data,
+        #     1
+        # )
         await test_ex.get_all(session)
-        await test_ex.get_or_404(1, session)
 
 
 asyncio.run(main())
