6c60db590ab8c141a6bee6b4d551857c3286f746
nepa27 <nepalek@mail.ru>
2025-09-15 13:18:11 +0300

    Add model of user

---
+++ src/core/users/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from src.core.base.models import Base
+
+
+class User(Base):
+    """Модель пользователя."""
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=False, unique=True)
+    second_name = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=False)
+
+    chat_data = relationship('ChatData', back_populates='users')
+    submits = relationship('Submit', back_populates='users')
