5cafa7494b95b7b448848a50d2ae82c7435baf88
nepa27 <nepalek@mail.ru>
2025-09-16 11:32:20 +0300

    Add async_session in every method

--- src/core/base/crud_base.py
+++ src/core/base/crud_base.py
@@ -2,11 +2,12 @@ from typing import Type, Sequence
 
 from fastapi import HTTPException, status
 from sqlalchemy import select, update
-from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.sql import text
 
+from src.core.base.models import Base
 from src.core.config.logging import logger
-from src.core.models import Base
+from src.core.db.database import async_session
 
 
 class CRUDBase:
@@ -21,81 +22,67 @@ class CRUDBase:
         """
         self.model = model
 
-    async def get_all(
-            self,
-            session: AsyncSession
-    ) -> Sequence[Base]:
+    async def get_all(self) -> Sequence[Base]:
         """Получает данные из БД."""
-        query = await session.execute(select(self.model))
-        result = query.scalars().all()
-        return result
+        async with async_session() as session:
+            query = await session.execute(select(self.model))
+            result = query.scalars().all()
+            return result
 
-    async def get_or_404(
-            self,
-            session: AsyncSession,
-            obj_id: int
-    ) -> Base:
+    async def get_or_404(self, obj_id: int) -> Base:
         """Получает объект из БД по id или выбрасывает 404-ошибку."""
-        query = await session.execute(
-            select(self.model).where(self.model.id == obj_id)
-        )
-        result = query.scalars().first()
-        if not result:
-            message = f'Объект {obj_id} в {self.model.__tablename__} не найден'
-            logger.error(message)
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=message)
-        return result
+        async with async_session() as session:
+            query = await session.execute(
+                select(self.model).where(self.model.id == obj_id)
+            )
+            result = query.scalars().first()
+            if not result:
+                message = f'Объект {obj_id} в {self.model.__tablename__} не найден'
+                logger.error(message)
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=message)
+            return result
 
-    async def create(
-            self,
-            session: AsyncSession,
-            data: dict
-    ) -> Base:
+    async def create(self, data: dict) -> Base:
         """Добавляет данные в БД."""
         db_obj = self.model(**data)
-        try:
-            session.add(db_obj)
-            await session.commit()
-            await session.refresh(db_obj)
+        async with async_session() as session:
+            try:
+                session.add(db_obj)
+                await session.commit()
+                await session.refresh(db_obj)
 
-            return db_obj
-        except Exception as e:
-            logger.error(f'Ошибка при добавлении данных в БД: {e}')
-            await session.rollback()
+                return db_obj
+            except Exception as e:
+                logger.error(f'Ошибка при добавлении данных в БД: {e}')
+                await session.rollback()
 
-    async def update(
-            self,
-            session: AsyncSession,
-            data: dict,
-            obj_id: int
-    ) -> None:
+    async def update(self, data: dict, obj_id: int) -> None:
         """Обновляет данные в БД."""
-        try:
-            query = (
-                update(self.model)
-                .values(**data)
-                .filter_by(id=obj_id)
-            )
-            await session.execute(query)
-            await session.commit()
-        except Exception as e:
-            logger.error(f'Ошибка при обновлении данных в БД: {e}')
-            await session.rollback()
+        async with async_session() as session:
+            try:
+                query = (
+                    update(self.model)
+                    .values(**data)
+                    .filter_by(id=obj_id)
+                )
+                await session.execute(query)
+                await session.commit()
+            except Exception as e:
+                logger.error(f'Ошибка при обновлении данных в БД: {e}')
+                await session.rollback()
 
-    async def delete(
-            self,
-            session: AsyncSession,
-            db_object: Base
-    ) -> None:
+    @staticmethod
+    async def delete(db_object: Base) -> None:
         """Удаляет данные из БД."""
-        try:
-            await session.delete(db_object)
-            await session.commit()
-        except Exception as e:
-            logger.error(f'Ошибка при удалении данных в БД: {e}')
-            await session.rollback()
+        async with async_session() as session:
+            try:
+                await session.delete(db_object)
+                await session.commit()
+            except Exception as e:
+                logger.error(f'Ошибка при удалении данных в БД: {e}')
+                await session.rollback()
 
     @staticmethod
     async def check_db_connection(engine: AsyncEngine) -> None:
