590fcdd36b16be2c3b77dafb7901b6cdaa84a444
nepa27 <nepalek@mail.ru>
2025-09-17 15:11:28 +0300

    Add routers for submit

---
+++ src/core/submits/routers.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+
+from src.core.submits.models import Submit
+from src.core.submits.schemas import SubmitGetSchema, SubmitCreateSchema
+from src.core.submits.services import SubmitService
+
+router = APIRouter(
+    prefix='/submits',
+    tags=['Отклики']
+)
+
+
+@router.get('')
+async def get_submits() -> list[SubmitGetSchema]:
+    """Возвращает все отклики."""
+    return await SubmitService(Submit).get_all()
+
+
+@router.get('/{submit_id}')
+async def get_one_submit(submit_id: int) -> SubmitGetSchema | None:
+    """Возвращает один отклик."""
+    data = await SubmitService(Submit).get_one_or_none(submit_id)
+    return data
+
+
+@router.post('/add')
+async def add_submit(user_data: SubmitCreateSchema):
+    """Добавляет отклик."""
+    return await SubmitService(Submit).create(user_data.dict())
